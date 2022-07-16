a8a73d7c5d500a3f32078cd163e387caa1a41cf3
kylian97 <kyky972@icloud.com>
2022-07-15 22:33:54 -0400

    first commit

---
+++ projet/Travel/test3.js
@@ -0,0 +1,9 @@
+let toggle = document.querySelector('.toggle');
+let body = document.querySelector ('body');
+
+toggle.addEventListener('click', function() {
+ body.classList.toggle('open')
+
+
+
+})
